bece1459613799247558e2e37ebed16bf3690ee9
Sinval Vieira <sinvalneto01@gmail.com>
2015-08-13 21:18:15 +0000

    [WIP] Sending oneview_client
    Change-Id: I8c7457157f6d5a2599cccb9e86be240fc4283767

---
+++ oneview_client/driver_oneview_exceptions.py
@@ -0,0 +1,56 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright 2015 Hewlett-Packard Development Company, L.P.
+# Copyright 2015 Universidade Federal de Campina Grande
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+class OneViewNotAuthorizedException(Exception):
+    message = ("Unauthorized access to OneView. Check credentials in"
+               " ironic.conf.")
+
+
+class OneViewResourceNotFoundError(Exception):
+    message = ("Resource not Found in OneView")
+
+
+class OneViewServerProfileTemplateError(Exception):
+    message = ("Server Profile Template not found into driver_info")
+
+
+class OneViewServerProfileCloneError(Exception):
+    message = ("Error cloning server profile")
+
+
+class OneViewMaxRetriesExceededError(Exception):
+    message = ("Max connection retries to OneView exceeded")
+
+
+class OneViewBootDeviceInvalidError(Exception):
+    message = ("The device is not valid to setup the boot order")
+
+
+class OneViewServerProfileAssociatedError(Exception):
+    message = ("There is no Server Profile associated to this Server"
+               " Hardware")
+
+
+class OneViewErrorStateSettingPowerState(Exception):
+    message = ("Get Error State in OneView trying to set power state of"
+               " Server Hardware")
+
+
+class OneViewTaskError(Exception):
+    message = ("The task for this action in OneView returnet an error state")
