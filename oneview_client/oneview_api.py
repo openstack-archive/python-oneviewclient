d78a864703ec56442870bdfc09196065b323b76d
afaranha <afaranha@lsd.ufcg.edu.br>
2016-03-18 00:03:29 +0000

    Started refactoring OneView Client
    Change-Id: I859587dc7edc8e62bcc35a20cbe5389bd1393976

---
+++ oneview_client/oneview_api.py
@@ -0,0 +1,109 @@
+import requests
+import json
+
+
+requests.packages.urllib3.disable_warnings()
+
+ONEVIEW_REST_API_VERSION = '200'
+
+GET_REQUEST_TYPE = 'GET'
+PUT_REQUEST_TYPE = 'PUT'
+POST_REQUEST_TYPE = 'POST'
+DELETE_REQUEST_TYPE = 'DELETE'
+
+AUTHENTICATION_URI = '/rest/login-sessions'
+SERVER_HARDWARE_PREFIX_URI = '/rest/server-hardware/'
+SERVER_PROFILE_TEMPLATE_PREFIX_URI = '/rest/server-profile-templates/'
+
+
+class OneViewRequestAPI(object):
+    def __init__(
+        self, manager_url, username, password,
+        allow_insecure_connections=True, tls_cacert_file='',
+        max_polling_attempts=20
+    ):
+        self.manager_url = manager_url
+        self.username = username
+        self.password = password
+        self.allow_insecure_connections = allow_insecure_connections
+        self.tls_cacert_file = tls_cacert_file
+        self.max_polling_attempts = max_polling_attempts
+
+        self.session_id = self._get_new_token()
+
+    def _get_ca_certificate(self):
+        if not self.allow_insecure_connections:
+            return self.tls_cacert_file
+        return False
+
+    def _get_new_token(self):
+        uri = "%s%s" %(self.manager_url, AUTHENTICATION_URI)
+        headers = {'content-type': 'application/json'}
+        body = {
+            'password': self.password,
+            'userName': self.username
+        }
+        response = requests.request(
+            POST_REQUEST_TYPE,  uri, headers=headers, data=json.dumps(body),
+            verify=self._get_ca_certificate()
+        )
+        return response.json()['sessionID']
+
+    def prepare_and_execute_request(self, uri, body={}, request_type='GET'):
+        headers = {
+            'content-type': 'application/json',
+            'X-Api-Version': ONEVIEW_REST_API_VERSION,
+            'Auth': self.session_id
+        }
+        url = '%s%s' % (self.manager_url, uri)
+        response = requests.request(
+            request_type, url, headers=headers, data=json.dumps(body),
+            verify=self._get_ca_certificate()
+        )
+        return response.json()
+
+
+class OneViewResourceAPI(OneViewRequestAPI):
+    resource_prefix_uri = None
+
+    def create(self, body):
+        return self.prepare_and_execute_request(
+            self.resource_prefix_uri, body=body, request_type=POST_REQUEST_TYPE
+        )
+
+    def list(self):
+        items_dict = self.prepare_and_execute_request(self.resource_prefix_uri)
+        return items_dict['members']
+
+    def get(self, uri):
+        return self.prepare_and_execute_request(uri)
+
+
+class OneViewServerHardware(OneViewResourceAPI):
+    resource_prefix_uri = SERVER_HARDWARE_PREFIX_URI
+
+
+class OneViewNetwork(OneViewResourceAPI):
+    resource_prefix_uri = NETWORK_PREFIX_URI
+
+
+class OneViewAPI(object):
+    def __init__(
+        self, manager_url, username, password,
+        allow_insecure_connections=True, tls_cacert_file='',
+        max_polling_attempts=20
+    ):
+        self.server_hardware = OneViewServerHardware(
+            manager_url=manager_url, username=username, password=password,
+            allow_insecure_connections=allow_insecure_connections,
+            tls_cacert_file=tls_cacert_file
+        )
+
+#url = ""
+#c = OneViewRequestAPI(url, '', '')
+#sh = OneViewServerHardware(url, '', '')
+#print sh.get()
+#body = {'userName': '', 'password': ''}
+#headers = {'content-type': 'application/json'}
+#print requests.post(
+#    "/rest/login-sessions", headers=headers, data=json.dumps(body), verify=False).json()
---
+++ oneview_client/oneview_objects.py
@@ -0,0 +1,123 @@
+import abc
+import six
+import oneview_api
+
+class Resource(object):
+    six.add_metaclass(abc.ABCMeta)
+
+    def __init__(self, info):
+        self._info = info
+        self._add_details(info)
+
+    def _add_details(self, info):
+        for (k, v) in six.iteritems(info):
+            try:
+                setattr(self, k, v)
+                self._info[k] = v
+            except AttributeError:
+                pass
+
+
+class Network(Resource):
+    def __repr__(self):
+        return "<Network uri='%s'>" % self.uri
+
+class ServerHardware(Resource):
+    def __repr__(self):
+        return "<ServerHardware uri='%s'>" % self.uri
+
+
+class Manager(object):
+    six.add_metaclass(abc.ABCMeta)
+    api = None
+
+    def __init__(self, oneview_api):
+        self.oneview_api = oneview_api
+
+    def _dict_to_object(self, dictionary):
+        raise Exception('Not Implemented')
+
+    def _object_to_dict(self, obj):
+        raise Exception('Not Implemented')
+
+    def create(self, obj):
+        self.api.create(_object_to_dict(obj))
+
+    def list(self):
+        objects = []
+        items = self.api.list()
+
+        for item in items:
+            objects.append(self._dict_to_object(item))
+
+        return objects
+
+    def get(self, uri):
+        return self._dict_to_object(self.api.get(uri))
+
+
+class NetworkManager(Manager):
+    fields = ['uri', 'name']
+
+    def _dict_to_object(self, dict):
+        filtered_server_profile_dict = {}
+        for field in self.fields:
+            filtered_server_profile_dict[field] = dict[field]
+        return Network(filtered_server_profile_dict)
+
+
+class ServerHardwareManager(Manager):
+    fields = ['uri', 'serverHardwareTypeUri', 'uuid', 'memoryMb',
+              'description', 'serverGroupUri', 'name', 'serverProfileUri']
+
+    def __init__(self, oneview_api):
+        super(ServerHardwareManager, self).__init__(oneview_api)
+        self.api = self.oneview_api.server_hardware
+
+    def create(self, obj):
+        raise Exception('Not Supported')
+
+    def _object_to_dict(self, obj):
+        pass
+
+    def _dict_to_object(self, dict):
+        filtered_server_profile_dict = {}
+        for field in self.fields:
+            filtered_server_profile_dict[field] = dict[field]
+
+        """
+        filtered_server_profile_dict['cpus'] = dict['processorCoreCount'] * \
+            dict['processorCount']
+        filtered_server_profile_dict['cpu_arch'] = 'x86_64'
+        filtered_server_profile_dict['local_gb'] = 120
+
+        filtered_server_profile_dict['serverHardwareTypeName'] = \
+            self.oneviewclient.server_hardware_type.get(
+                filtered_server_profile_dict['serverHardwareTypeUri'], 'name')
+        filtered_server_profile_dict['serverGroupName'] = \
+            self.oneviewclient.enclosure_group.get(
+                filtered_server_profile_dict['serverGroupUri'], 'name')
+
+        print filtered_server_profile_dict
+        """
+        return ServerHardware(filtered_server_profile_dict)
+
+
+class OneViewClient(object):
+    def __init__(
+        self, manager_url, username, password,
+        allow_insecure_connections=False, tls_cacert_file=''
+    ):
+        self.oneview_api = oneview_api.OneViewAPI(
+            manager_url=manager_url, username=username, password=password,
+            allow_insecure_connections=allow_insecure_connections,
+            tls_cacert_file=tls_cacert_file
+        )
+        self.server_hardware = ServerHardwareManager(self.oneview_api)
+        self.network = NetworkManager(self.oneview_api)
+
+
+url = ""
+c = OneViewClient(url, '', '')
+l = c.server_hardware.list()
+print c.server_hardware.get(l[0].uri)
